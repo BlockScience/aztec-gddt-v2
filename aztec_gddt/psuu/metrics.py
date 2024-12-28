fe7780734a1ac8514a4848d40d00442ff9eeee7b
Danilo Lessa Bernardineli <danilo.lessa@gmail.com>
2024-12-28 17:48:11 -0300

    add tg metrics

--- aztec_gddt/psuu/metrics.py
+++ aztec_gddt/psuu/metrics.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from aztec_gddt.types import *
 
 def relative_volatility_across_time(traj_df: pd.DataFrame) -> float:
@@ -62,6 +63,27 @@ PER_TRAJECTORY_METRICS = {
     'T-M9': network_resumed_finalization_following_inactivity,
 }
 
+
+def avg_over_fn(group_traj_dfs: list[pd.DataFrame], fn):
+    avgs = []
+    for traj_df in group_traj_dfs:
+        avgs.append(fn(traj_df))
+    return np.mean(avgs)
+
+
 PER_TRAJECTORY_GROUP_METRICS = {
+    'TG-M1': lambda dfs: avg_over_fn(dfs, relative_volatility_across_time),
+    'TG-M2': lambda dfs: avg_over_fn(dfs, empty_blocks_during_trajectory),
+    'TG-M3': lambda dfs: avg_over_fn(dfs, unproven_epochs_during_trajectory),
+    'TG-M4': lambda dfs: avg_over_fn(dfs, fraction_dropped_tx_during_trajectory),
+    'TG-M5': lambda dfs: float('nan'), # TODO
+    'TG-M6': lambda dfs: avg_over_fn(dfs, fraction_excluded_tx_during_trajectory),
+    'TG-M7': lambda dfs: avg_over_fn(dfs, base_fee_rebound_inside_range),
+    'TG-M8': lambda dfs: avg_over_fn(dfs, base_fee_divided_by_oracle_parameter),
+    'TG-M9': lambda dfs: avg_over_fn(dfs, counterfactual_sequencer_losses_due_to_lag),
+    'TG-M10': lambda dfs: float('nan'), # TODO
+    'TG-M11': lambda dfs: float('nan'), # TODO
+    'TG-M12': lambda dfs: float('nan'), # TODO
+    'TG-M13': lambda dfs: float('nan'), # TODO
 
 }
